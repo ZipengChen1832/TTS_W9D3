0f0818244ca2760b909f1b6599a047277c4f6525
Zipeng <zipengchen1832@gmail.com>
2021-08-23 02:54:52 -0700

    first commit

---
+++ src/main/java/W9D3/W9D3Application.java
@@ -0,0 +1,13 @@
+package W9D3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W9D3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W9D3Application.class, args);
+	}
+
+}
---
+++ src/test/java/W9D3/W9D3ApplicationTests.java
@@ -0,0 +1,13 @@
+package W9D3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W9D3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
